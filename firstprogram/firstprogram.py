1f4948a9c3edaffbfdeedf7c16a818ac93b3d983
Ben <d3boy2013@gmail.com>
2021-02-14 23:26:19 -0500

    firstprogram

---
+++ firstprogram/firstprogram.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 14 20:41:52 2021
+
+@author: benedictpark
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy as sp
+import random
+import math
+from scipy.stats import beta
+from collections import namedtuple
+import os
+import shutil
+import csv
+
